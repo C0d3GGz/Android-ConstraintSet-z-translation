88e1b50df622f795f4c707dd960cfd714ba986c9
David Petersen <david@familiep.de>
2018-12-13 16:51:01 +0100

    migration to android x

--- app/src/main/java/example/constraintsetzvaluebug/MainActivity.kt
+++ app/src/main/java/example/constraintsetzvaluebug/MainActivity.kt
@@ -1,8 +1,8 @@
 package example.constraintsetzvaluebug
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
